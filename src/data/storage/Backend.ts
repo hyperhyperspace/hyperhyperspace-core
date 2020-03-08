114b42c40fc1e7953638cfd95f27f489b02bf442
Santiago Bazerque <sbazerque@gmail.com>
2020-03-08 17:14:34 -0300

    Added implementations for index-based load functions to IdbBackend.

--- src/data/storage/Backend.ts
+++ src/data/storage/Backend.ts
@@ -1,12 +1,16 @@
 import { Hash } from 'data/model/Hashing';
 import { PackedLiteral } from 'data/storage/Store';
 
+type SearchParams = {order?: string, start?: string, limit?: number};
+type SearchResults = {items : Array<PackedLiteral>, start?: string, end?: string };
 
 interface Backend {
 
     store(packed : PackedLiteral) : Promise<void>;
     load(hash: Hash) : Promise<PackedLiteral | undefined>;
 
+    searchByClass(className: string, params? : SearchParams) : Promise<SearchResults>;
+    searchByReference(path: string, refHash: Hash, params? : SearchParams) : Promise<SearchResults>;
 }
 
-export { Backend }
+export { Backend, SearchParams, SearchResults }
--- src/data/storage/backends/IdbBackend.ts
+++ src/data/storage/backends/IdbBackend.ts
@@ -1,6 +1,6 @@
 require('indexeddbpoly');
 
-import { Backend } from 'data/storage/Backend'; 
+import { Backend, SearchParams, SearchResults } from 'data/storage/Backend'; 
 import { PackedLiteral } from 'data/storage/Store';
 
 import { openDB, IDBPDatabase } from 'idb';
@@ -80,6 +80,69 @@ class IdbBackend implements Backend {
         let idb = await this.idbPromise;
 
         return idb.get(IdbBackend.OBJ_STORE, hash).then((storable: IdbStorageFormat | undefined) => (storable?.packed)) as Promise<PackedLiteral | undefined>;
+    }
+
+    async searchByClass(className: string, params?: SearchParams): Promise<SearchResults> {
+        return this.searchByIndex(IdbBackend.CLASS_TIMESTAMP_IDX_KEY + '_idx', className, params);
+    }
+
+    async searchByReference(path: string, refHash: string, params?: SearchParams): Promise<SearchResults> {
+        return this.searchByIndex(IdbBackend.REFERENCE_TIMESTAMPS_IDX_KEY + '_idx', path + '#' + refHash, params);
+    }
+
+    private async searchByIndex(index: string, value: string, params?: SearchParams) : Promise<SearchResults> {
+        
+        let idb = await this.idbPromise;
+
+        let order = (params === undefined || params.order === undefined) ? 'asc' : params.order.toLowerCase();
+        let range_start = null;
+        let range_end   = null;
+
+        if (order === 'asc') {
+            if (params === undefined || params.start === undefined) {
+              range_start = value + '_';
+            } else {
+              range_start = params.start;
+            }
+            range_end = value + '_Z';
+        } else {
+            if (params === undefined || params.start === undefined) {
+              range_start = value + '_Z';
+            } else {
+              range_start = params.start;
+            }
+            range_end = value + '_';
+        }
+
+        const range = IDBKeyRange.bound(range_start, range_end, true, true);
+        const direction = order === 'asc' ? 'next' : 'prev';
+
+        let searchResults = {} as SearchResults;
+
+        searchResults.items = [] as Array<PackedLiteral>;
+        searchResults.start = undefined;
+        searchResults.end   = undefined;
+
+        let ingestCursor = async () => {
+      
+            var cursor = await idb.transaction([IdbBackend.OBJ_STORE], 'readonly').objectStore(IdbBackend.OBJ_STORE).index(index).openCursor(range, direction);
+      
+            const limit = params?.limit;
+
+            while ((limit === undefined || searchResults.items.length < limit) && cursor) {
+      
+                let storable = cursor.value as IdbStorageFormat;
+                searchResults.items.push(storable.packed);
+                if (searchResults.start === undefined) {
+                    searchResults.start = cursor.key.toString();
+                }
+                searchResults.end = cursor.key.toString();
+                
+                cursor = await cursor.continue();
+            }
+        }
+
+        return ingestCursor().then(() => searchResults);
     }
 
     private static assignIdxValue(storable: IdbStorageFormat, key: string, value: string, params?:{timestamp?: boolean, multi?: boolean}) {
