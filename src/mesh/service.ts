72c5378fc741dbb97fae0d4fa1ebf5fa0c40aa00
Santiago Bazerque <sbazerque@gmail.com>
2020-08-18 19:28:22 -0300

    exported SyncMode, added a shorthand class PeerGroup for clarity.

--- src/mesh/service.ts
+++ src/mesh/service.ts
@@ -1,3 +1,4 @@
-export { Mesh, PeerGroupInfo } from './service/Mesh';
+export { Mesh, PeerGroupInfo, SyncMode } from './service/Mesh';
 export { AgentPod, Event, AgentSetChangeEvent, AgentSetChange } from './service/AgentPod';
 export { Agent } from './service/Agent';
+export { PeerGroup } from './service/PeerGroup';
