2a0b613c4000822b7f5efa09c19176e6afb43754
joigno <30682875+joigno@users.noreply.github.com>
2020-12-28 21:49:42 +0100

    Copy of chat with minor changes.

---
+++ examples/model/Account.ts
@@ -0,0 +1,144 @@
+import { HashedObject, HashedLiteral } from 'data/model';
+import { MutableSet, MutableReference } from 'data/containers';
+import { Identity } from 'data/identity';
+
+import { Message } from './Message';
+
+import { SpaceEntryPoint } from 'spaces/SpaceEntryPoint';
+import { Mesh } from 'mesh/service';
+import { LinkupManager } from 'net/linkup';
+import { ObjectDiscoveryPeerSource } from 'mesh/agents/peer';
+import { PeerGroupInfo } from 'mesh/service';
+import { IdentityPeer } from 'mesh/agents/peer';
+
+
+class Account extends HashedObject implements SpaceEntryPoint {
+
+    static className = 'hhs/v0/exampes/ChatRoom';
+
+    topic?: MutableReference<HashedLiteral>
+    participants?: MutableSet<Identity>;
+    messages?: MutableSet<Message>;
+
+    _mesh?: Mesh;
+    _peerGroup?: PeerGroupInfo;
+
+    constructor(topic?: string) {
+        super();
+        if (topic !== undefined) {
+            this.setRandomId();
+
+            this.addDerivedField('topic', new MutableReference());
+            this.addDerivedField('participants', new MutableSet());
+            this.addDerivedField('messages', new MutableSet());
+
+            this.topic?.setValue(new HashedLiteral(topic));
+        }
+    }
+
+    init(): void {
+        
+    }
+
+    validate(_references: Map<string, HashedObject>): boolean {
+        return  this.getId() !== undefined &&
+                this.checkDerivedField('topic') &&
+                this.checkDerivedField('participants') &&
+                this.checkDerivedField('messages');
+    }
+
+    join(id: Identity) {
+        this.participants?.add(id);
+        this.getStore().save(this.participants as HashedObject);
+        //this.participants?.saveQueuedOps();
+    }
+
+    leave(id: Identity) {
+        this.participants?.delete(id);
+        this.participants?.saveQueuedOps();
+    }
+
+    say(author: Identity, text: string) {
+        let message = new Message(author, text);
+        this.messages?.add(message).then( () => {
+            this.getStore().save(this.messages as HashedObject);
+        })        
+        //this.messages?.saveQueuedOps();
+    }
+
+    getParticipants() : MutableSet<Identity> {
+        if (this.participants === undefined) {
+            throw new Error('The chat room has not been initialized, participants are unavailable.');
+        }
+
+        return this.participants;
+    }
+
+    getMessages() : MutableSet<Message> {
+        if (this.messages === undefined) {
+            throw new Error('The chat room has not been initialized, messages are unavailable.');
+        }
+
+        return this.messages;
+    }
+
+    async startSync(): Promise<void> {
+
+        let resources = this.getResources();
+
+        if (resources === undefined) {
+            throw new Error('Cannot start sync: resources not configured.');
+        }
+
+        this._mesh = resources.mesh;
+
+        if (this._mesh === undefined) {
+            throw new Error('Cannot start sync: mesh is missing from configured resources.');
+        }
+
+        let linkupServers = resources.config.linkupServers === undefined?
+                            [LinkupManager.defaultLinkupServer] : resources.config.linkupServer as string[];
+
+
+
+        const localIdentity = resources.config.id as Identity;
+        const localPeer     = await new IdentityPeer(linkupServers[0] as string, localIdentity.hash(), localIdentity).asPeer();
+
+        this._mesh.startObjectBroadcast(this, linkupServers, [localPeer.endpoint]);
+
+        let peerSource = new ObjectDiscoveryPeerSource(this._mesh, this, linkupServers, localPeer.endpoint, IdentityPeer.getEndpointParser(resources.store));
+
+        this._peerGroup = {
+            id: 'sync-for-' + this.hash(),
+            localPeer: localPeer,
+            peerSource: peerSource
+        }
+
+        this._mesh.joinPeerGroup(this._peerGroup);
+        this._mesh.syncObjectWithPeerGroup(this._peerGroup.id, this);
+
+        this.participants?.loadAndWatchForChanges();
+        this.messages?.loadAndWatchForChanges();
+    }
+    
+    async stopSync(): Promise<void> {
+
+        const peerGroupId = this._peerGroup?.id as string;
+        
+        this._mesh?.stopSyncObjectWithPeerGroup(peerGroupId, this.hash());
+        this._mesh?.stopObjectBroadcast(this.hash());
+        this._mesh?.leavePeerGroup(peerGroupId);
+
+        this._mesh = undefined;
+        this._peerGroup = undefined;
+    }
+
+    getClassName(): string {
+        return Account.className;
+    }
+
+}
+
+HashedObject.registerClass(Account.className, Account);
+
+export { Account as ChatRoom };
---
+++ examples/model/Feed.ts
@@ -0,0 +1,144 @@
+import { HashedObject, HashedLiteral } from 'data/model';
+import { MutableSet, MutableReference } from 'data/containers';
+import { Identity } from 'data/identity';
+
+import { Message } from './Message';
+
+import { SpaceEntryPoint } from 'spaces/SpaceEntryPoint';
+import { Mesh } from 'mesh/service';
+import { LinkupManager } from 'net/linkup';
+import { ObjectDiscoveryPeerSource } from 'mesh/agents/peer';
+import { PeerGroupInfo } from 'mesh/service';
+import { IdentityPeer } from 'mesh/agents/peer';
+
+
+class Feed extends HashedObject implements SpaceEntryPoint {
+
+    static className = 'hhs/v0/examples/Feed';
+
+    bio?: MutableReference<HashedLiteral>
+    posts?: MutableSet<Message>;
+
+    _mesh?: Mesh;
+    _peerGroup?: PeerGroupInfo;
+
+    constructor(author?: Identity) {
+        super();
+        if (author !== undefined) {
+            this.setId('-feed-'+author.hash());
+
+            this.addDerivedField('bio', new MutableReference());
+            
+            this.addDerivedField('posts', new MutableSet());
+            this.posts?.setAuthor(author);
+
+            this.setAuthor(author);
+        }
+    }
+
+    init(): void {
+        
+    }
+
+    validate(_references: Map<string, HashedObject>): boolean {
+        return  this.getId() !== undefined &&
+                // TODO: CHECKS
+                this.checkDerivedField('bio') &&
+                this.checkDerivedField('posts');
+    }
+
+    join(id: Identity) {
+        this.followers?.add(id);
+        this.getStore().save(this.followers as HashedObject);
+        //this.participants?.saveQueuedOps();
+    }
+
+    leave(id: Identity) {
+        this.followers?.delete(id);
+        this.followers?.saveQueuedOps();
+    }
+
+    say(author: Identity, text: string) {
+        let message = new Message(author, text);
+        this.posts?.add(message).then( () => {
+            this.getStore().save(this.posts as HashedObject);
+        })        
+        //this.messages?.saveQueuedOps();
+    }
+
+    getParticipants() : MutableSet<Identity> {
+        if (this.followers === undefined) {
+            throw new Error('The chat room has not been initialized, participants are unavailable.');
+        }
+
+        return this.followers;
+    }
+
+    getMessages() : MutableSet<Message> {
+        if (this.posts === undefined) {
+            throw new Error('The chat room has not been initialized, messages are unavailable.');
+        }
+
+        return this.posts;
+    }
+
+    async startSync(): Promise<void> {
+
+        let resources = this.getResources();
+
+        if (resources === undefined) {
+            throw new Error('Cannot start sync: resources not configured.');
+        }
+
+        this._mesh = resources.mesh;
+
+        if (this._mesh === undefined) {
+            throw new Error('Cannot start sync: mesh is missing from configured resources.');
+        }
+
+        let linkupServers = resources.config.linkupServers === undefined?
+                            [LinkupManager.defaultLinkupServer] : resources.config.linkupServer as string[];
+
+
+
+        const localIdentity = resources.config.id as Identity;
+        const localPeer     = await new IdentityPeer(linkupServers[0] as string, localIdentity.hash(), localIdentity).asPeer();
+
+        this._mesh.startObjectBroadcast(this, linkupServers, [localPeer.endpoint]);
+
+        let peerSource = new ObjectDiscoveryPeerSource(this._mesh, this, linkupServers, localPeer.endpoint, IdentityPeer.getEndpointParser(resources.store));
+
+        this._peerGroup = {
+            id: 'sync-for-' + this.hash(),
+            localPeer: localPeer,
+            peerSource: peerSource
+        }
+
+        this._mesh.joinPeerGroup(this._peerGroup);
+        this._mesh.syncObjectWithPeerGroup(this._peerGroup.id, this);
+
+        this.followers?.loadAndWatchForChanges();
+        this.posts?.loadAndWatchForChanges();
+    }
+    
+    async stopSync(): Promise<void> {
+
+        const peerGroupId = this._peerGroup?.id as string;
+        
+        this._mesh?.stopSyncObjectWithPeerGroup(peerGroupId, this.hash());
+        this._mesh?.stopObjectBroadcast(this.hash());
+        this._mesh?.leavePeerGroup(peerGroupId);
+
+        this._mesh = undefined;
+        this._peerGroup = undefined;
+    }
+
+    getClassName(): string {
+        return Feed.className;
+    }
+
+}
+
+HashedObject.registerClass(Feed.className, Feed);
+
+export { Feed as ChatRoom };
