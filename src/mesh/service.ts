68555e3f7edddf40c1941989802045a17057574a
Santi Bazerque <sbazerque@gmail.com>
2021-01-26 15:57:54 -0300

    remoting belongs inside services

--- src/mesh/service.ts
+++ src/mesh/service.ts
@@ -1,6 +1,6 @@
 export { Mesh, PeerGroupInfo, SyncMode } from './service/Mesh';
-export { MeshServer, MeshCommand, CommandStreamedReply} from './remoting/MeshServer';
-export { MeshClient } from './remoting/MeshClient';
+export { MeshServer, MeshCommand, CommandStreamedReply} from './service/remoting/MeshServer';
+export { MeshClient } from './service/remoting/MeshClient';
 export { AgentPod, Event, AgentSetChangeEvent, AgentSetChange } from './service/AgentPod';
 export { Agent } from './service/Agent';
 export { PeerGroup } from './service/PeerGroup';
