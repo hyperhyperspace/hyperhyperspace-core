3636b9ca8f0d0758ed55f1cf2a31c7317143e4c1
Santi Bazerque <sbazerque@gmail.com>
2021-04-09 21:04:45 -0300

    fixed perpetual gossip exchange

--- src/mesh/agents/state/StateGossipAgent.ts
+++ src/mesh/agents/state/StateGossipAgent.ts
@@ -118,7 +118,7 @@ class StateGossipAgent extends PeeringAgentBase {
         return 'state-gossip-agent-for-' + gossipId;
     }
 
-    static peerMessageLog = new Logger(StateGossipAgent.name, LogLevel.INFO);
+    static peerMessageLog = new Logger(StateGossipAgent.name, LogLevel.TRACE);
     static controlLog     = new Logger(StateGossipAgent.name, LogLevel.INFO);
 
     // tunable working parameters
@@ -486,7 +486,7 @@ class StateGossipAgent extends PeeringAgentBase {
 
         }
 
-        if (receivedOldState) {
+        if (receivedOldState && this.localState.get(agentId) !== state) {
             this.peerMessageLog.trace('Received old state for ' + agentId + ' from ' + sender + ', sending our own state over there.');
             this.sendStateObject(sender, agentId);
         }
