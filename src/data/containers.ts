fcf26661eeb1c63f49a0d00e68b512d003fc7f7e
Santi Bazerque <sbazerque@gmail.com>
2021-10-29 10:47:31 -0300

    exported ReversibleSet

--- src/data/containers.ts
+++ src/data/containers.ts
@@ -1,3 +1,4 @@
 export { MutableSet } from './containers/MutableSet';
 export { MutableReference } from './containers/MutableReference';
+export { ReversibleSet } from './containers/ReversibleSet';
 export * from './containers/CapabilitySet';
