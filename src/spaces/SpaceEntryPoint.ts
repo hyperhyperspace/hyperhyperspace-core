c52f221d228424c08eea70a388dc9c20519b9e35
Santi Bazerque <sbazerque@gmail.com>
2022-12-11 01:09:12 -0300

    add a version string to SpaceEntryPoints

--- src/spaces/SpaceEntryPoint.ts
+++ src/spaces/SpaceEntryPoint.ts
@@ -1,9 +1,13 @@
 import { MutableReference } from 'data/collections';
 
 interface SpaceEntryPoint {
+    
     getName(): MutableReference<string>|string|undefined;
+    getVersion(): string;
+
     startSync(): Promise<void>;
     stopSync(): Promise<void>;
+    
 }
 
 export { SpaceEntryPoint }
